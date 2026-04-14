f8ef8e6237b3bf84a0f931cafdaa9fa28958f7f2
Kacper Chwiedor <106623070+KacChw@users.noreply.github.com>
2026-04-14 15:00:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,5 +9,12 @@ class_names = np.array(['setosa', 'versicolor','virginica'])
 app =FastAPI()
 
 @app.get('/')
+def reed_root():
+  return{'message':'Iris model API'}
 
 @app.post('/predict')
+def predict(data: dict):
+  features = np.array(data['features']).reshape(1,-1)
+  prediction = model.predict(features)
+  class_name = class_names[prediction][0]
+  return {'predicted_class':class_name}
