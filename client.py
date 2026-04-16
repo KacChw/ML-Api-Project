1e495ea5e6127d06502ad7bb0af77e1ecb69cb8e
Kacper Chwiedor <106623070+KacChw@users.noreply.github.com>
2026-04-16 15:46:25 +0200

    predictions as table

--- client.py
+++ client.py
@@ -5,6 +5,11 @@ data = {'features': [1,2,3,4]}
 
 url = 'http://0.0.0.0:8000/predict/'
 
-data = json.dumps(data)
-response = requests.post(url, data)
-print(response.json())
+predictions = []
+for record in data:
+  payload = {'features':record}
+  payload = json.dumps(payload)
+  response = requests.post(url, data=payload)
+  predictions.append(response.json()['predictions])
+
+print(predictions)
